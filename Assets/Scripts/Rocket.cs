6ff540affb8c91c58bfcf0d98d9d6bb5261cbad8
Karlsson0214 <karlsson0214@gmail.com>
2022-10-04 08:17:16 +0200

    Rocket outside screen => reappear on opposite side.
    More fields.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Sprite rocket;
+    [SerializeField] private Sprite rocketWithFlame;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private float xMax;
     private float yMax;
+    private float rotationSpeed = 120f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
         // camera center to top of screen in world coordinates
         yMax = Camera.main.orthographicSize;
         xMax = yMax * Camera.main.aspect;
@@ -20,23 +26,48 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
+            // forward
             rb.AddForce(transform.right);
+            sr.sprite = rocketWithFlame;
+        }
+        else
+        {
+            sr.sprite = rocket;
+
         }
         if (Input.GetKey(KeyCode.A))
         {
             // turn left
-            rb.SetRotation(rb.rotation + 120f * Time.deltaTime); 
+            rb.SetRotation(rb.rotation + rotationSpeed * Time.deltaTime); 
         }
         else if (Input.GetKey(KeyCode.D))
         {
             // turn right
-            rb.SetRotation(rb.rotation - 120f * Time.deltaTime);
+            rb.SetRotation(rb.rotation - rotationSpeed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // shoot
+        }
+
+        // ouside screen => appear on opposite side
         if (rb.position.x > xMax)
         {
             rb.position = new Vector2(-xMax, rb.position.y);
+        }
+        else if (rb.position.x < -xMax)
+        {
+            rb.position = new Vector2(xMax, rb.position.y);
+        }
+        else if (rb.position.y > yMax)
+        {
+            rb.position = new Vector2(rb.position.x, -yMax);
+        }
+        else if (rb.position.y < -yMax)
+        {
+            rb.position = new Vector2(rb.position.x, yMax);
         }
         
     }
